8742d5f279c9587da1317b62f7469d4d36764202
이규정 <veracro97@gmail.com>
2026-04-20 17:38:59 +0900

    parse_uri

--- webproxy-lab/tiny/tiny.c
+++ webproxy-lab/tiny/tiny.c
@@ -145,4 +145,102 @@ void read_requesthdrs(rio_t *rp)
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
                  char *longmsg)
 {
+  char buf[MAXLINE], body[MAXBUF];
+
+  /* Build the HTTP response body */
+  sprintf(body, "<html><title>Tiny Error</title>");
+  sprintf(body, "%s<body bgcolor=\"ffffff\">\r\n", body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+
+  /* Print the HTTP response */
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-type: text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+  Rio_writen(fd, buf, strlen(buf));
+  Rio_writen(fd, body, strlen(body));
+}
+
+// uri를 파싱하는 함수
+int parse_uri(char *uri, char *filename, char *cgiargs)
+{
+  char *ptr;
+
+  // strstr() 함수 : 1번째 인자 -> 긴 문자열 전체, 2번째 인자 -> 그 안에서 찾고 싶은 문자열
+  // 즉, uri에 "cgi-bin" 이 없다면 -> static 콘텐츠
+  if (!strstr(uri, "cgi-bin"))
+  {
+    // 정적 컨텐츠이므로 CGI 인자가 필요없다.
+    strcpy(cgiargs, "");
+    // filename 에는 ./url ~ 이 들어간다.
+    strcpy(filename, ".");
+    strcat(filename, uri);
+
+    // 마지막 글자가 '/' 임을 확인하는 조건문
+    if (uri[strlen(uri) - 1] == '/')
+      strcat(filename, "home.html");
+    /*
+    왜 위 조건문과 home.html을 붙이는 작업이 필요할까?
+    예를 들어 사용자가 요청한 url이 http://localhost:8000/ 이라고 생각해보자.
+    위 같은 경우에 뒤에 파일명을 안 쓰고 /만 요청하면 default 로 home.html을 보여주기 위한 장치.
+    */
+    return 1;
+  }
+
+  // 동적 콘텐츠인 경우
+  else
+  { /* Dynamic content */
+    // ptr에 uri의 '?' 문자의 위치를 저장
+    ptr = index(uri, '?');
+    // 만약 ? 문자가 있다면 인자가 있는것이므로 뒤의 인자를 앞에 uri와 분리시켜줘야함.
+    if (ptr)
+    {
+      // ptr 다음의 문자열을 cgiargs에 넣어주고
+      strcpy(cgiargs, ptr + 1);
+      // ptr이 가리키고 있는 자리의 문자를 문자열의 종료를 알려주는 문자 '\0'으로 교체함.
+      *ptr = '\0';
+    }
+
+    // CGI 인자가 없는 경우에는 cgiargs 를 빈 문자열로.
+    else
+      strcpy(cgiargs, "");
+    // 나머지 부분은 정적 콘텐츠 부분과 동일하다.
+    strcpy(filename, ".");
+    strcat(filename, uri);
+    return 0;
+  }
+  // parse_uri 의 반환값은 정적 콘텐츠 일때는 1, 동적 콘텐츠 일때는 0을 반환한다.
+}
+
+// static 요청 처리 함수
+void serve_static(int fd, char *filename, int filesize)
+{
+  int srcfd;
+  char *srcp, filetype[MAXLINE], buf[MAXBUF];
+
+  /* Send response headers to client */
+  get_filetype(filename, filetype);
+  sprintf(buf, "HTTP/1.0 200 OK\r\n");
+  sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
+  sprintf(buf, "%sConnection: close\r\n", buf);
+  sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+  sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
+  Rio_writen(fd, buf, strlen(buf));
+  printf("Response headers:\n");
+  printf("%s", buf);
+
+  /* Send response body to client */
+  srcfd = Open(filename, O_RDONLY, 0);
+  srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+  Close(srcfd);
+  Rio_writen(fd, srcp, filesize);
+  Munmap(srcp, filesize);
+}
+
+// dynamic 요청 처리 함수
+void serve_dynamic(int fd, char *filename, char *cgiargs)
+{
 }
